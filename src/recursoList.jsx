905fc63f950d57c3f82c387799f289fdb5174fd6
tarea5ejercicio <olivarmeylinxolimar@.com>
2023-07-10 15:54:15 -0600

    comentariosR

--- src/recursoList.jsx
+++ src/recursoList.jsx
@@ -2,10 +2,20 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import {useRoute} from '@react-navigation/native';
 import { recursosAprendizaje as data } from './db';
+// En esta sección se importan los módulos necesarios de React, React Native y otros.
+//React` y `useState` son importados de la biblioteca 'react'.
+//View`, `Text`, `StyleSheet`, `FlatList` y `TouchableOpacity` son componentes proporcionados por React Native para construir interfaces de usuario.
+//useRoute` se importa de la biblioteca '@react-navigation/native', que se utiliza para obtener la ruta actual en la navegación.
+//recursosAprendizaje` se importa de './db'. Presumiblemente, es un conjunto de datos que contiene recursos de aprendizaje.
+
 
 const RecursoList = ({navigation}) => {
   const route = useRoute();
   const campo = route.params?.campo;
+//Aquí se define un componente de función llamado `RecursoList`. Recibe la prop `navigation` que se utilizará para navegar a la pantalla 'DetalleCurso'.
+//useRoute` se utiliza para obtener la ruta actual y se asigna a la variable `route`.
+// La variable `campo` se inicializa con el valor de `route.params?.campo`. Esto significa que `campo` obtendrá el valor del parámetro 'campo' de la ruta actual si existe, de lo contrario, será `undefined`.
+
 
   const RenderCampoItem = ({item}) => {
     return (
@@ -21,6 +31,10 @@ const RecursoList = ({navigation}) => {
           </View>
         ) : null}
       </>
+//-`RenderCampoItem` es un componente de función que se encarga de renderizar los elementos relacionados con un campo específico.
+//Si `campo` es igual al campo del elemento actual (`item.campo`), se muestra un `View` con el título del campo (`item.campo`) y una lista de recursos utilizando `FlatList`. El componente `RenderRecursoItem` se utiliza para renderizar cada recurso de la lista.
+//Si `campo` no coincide con el campo del elemento actual, se muestra `null`.
+
     );
   };
 
@@ -33,6 +47,11 @@ const RecursoList = ({navigation}) => {
         <Text style={styles.recursoItemTitle}>{item.titulo}</Text>
       </TouchableOpacity>
     );
+//`RenderRecursoItem` es un componente de función que renderiza un recurso individual.
+//Utiliza `TouchableOpacity` para crear un área de presionar sobre el recurso.
+//Cuando se presiona un recurso, se navega a la pantalla 'DetalleCurso' y se pasa el objeto `item` como parámetro con la clave 'newsItem'.
+//Se muestra el tipo del recurso (`item.tipo`) y el título (`item.titulo`) dentro del componente `TouchableOpacity`.
+
   };
 
   return (
@@ -47,10 +66,15 @@ const RecursoList = ({navigation}) => {
         keyExtractor={(item, index) => index.toString()}
       />
     </View>
+//- El componente `RecursoList` devuelve una vista contenedora con estilo `styles.container`.
+//Se utiliza `FlatList` para mostrar una lista de elementos a partir de los datos proporcionados en `data`.
+//Para cada elemento de `data`, se renderiza un componente `TouchableOpacity` envolviendo el componente `RenderCampoItem`, pasando el elemento como prop `item`.
+//`keyExtractor` se utiliza para asignar una clave única a cada elemento de la lista.
+
   );
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({//Estilos CSS para los componentes
   container: {
     flex: 1,
     paddingTop: 20,
@@ -119,6 +143,10 @@ const styles = StyleSheet.create({
   filterButtonText: {
     color: 'white',
   },
+  //Aquí se define un objeto `styles` 
+  //que contiene estilos CSS utilizando la función `StyleSheet.create()` de React Native.
 });
 
 export default RecursoList;
+//Se exporta el componente `RecursoList` como el valor predeterminado del módulo. 
+//Esto permitirá que otros módulos lo importen y lo utilicen.
